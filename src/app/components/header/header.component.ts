3e0d9d48aef7c120d2560277d1bf2e8f70a5f8d1
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-03-24 21:06:04 +0000

    initial settings + tailwind css installation

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <header class="ng-slate-100 px-4 py-3 shadow-md">
+      <span>Store</span>
+    </header>
+  `,
+  styles: ``,
+})
+export class HeaderComponent {}
