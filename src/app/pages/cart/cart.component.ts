6b649362c2929dcf547f6d595eba71bfbab72778
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-03-28 11:58:33 +0000

    created order summary

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,11 @@
 import { CartService } from './../../services/cart.service';
 import { Component, inject } from '@angular/core';
 import { CartItemComponent } from './cart-item/cart-item.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, OrderSummaryComponent],
   template: `
     <h2 class="text-2xl">Shopping Cart</h2>
     <ul class="p-6 flex flex-col gap-4">
@@ -12,6 +13,7 @@ import { CartItemComponent } from './cart-item/cart-item.component';
       <app-cart-item [cartItem]="cartItem" />
       }
     </ul>
+    <app-order-summary />
   `,
   styles: ``,
 })
